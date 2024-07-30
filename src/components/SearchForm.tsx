fa62d665885eaf77db4b07c957f419160b57cf2d
mJiga <guillermojiga10@gmail.com>
2024-07-30 16:26:07 -0600

    states added

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,13 @@
 import { Search } from 'lucide-react';
+import { useState } from 'react';
 
 function SearchForm() {
+
+  const [desination, setDestination] = useState('');
+  const [arrival, setArrival] = useState('');
+  const [departure, setDeparture] = useState('');
+  
+  
   return (
     <div className="border-2 border-opacity-60 border-zinc-300 relative flex items-center max-w-4xl h-20 rounded-3xl shadow-lg overflow-hidden">
       <form className='flex w-full h-full items-center'>
@@ -8,6 +15,7 @@ function SearchForm() {
           <span className="text-sm font-extrabold">Destination</span>
           <input
             placeholder='Where to go?'
+            value={desination}
             className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
             required
           />
@@ -17,6 +25,7 @@ function SearchForm() {
           <span className="text-sm font-extrabold">Arrival</span>
           <input
             placeholder='-- / -- / --'
+            value={arrival}
             className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
             required
           />
@@ -26,6 +35,7 @@ function SearchForm() {
           <span className="text-sm font-extrabold">Departure</span>
           <input
             placeholder='-- / -- / --'
+            value={departure}
             className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
             required
           />
