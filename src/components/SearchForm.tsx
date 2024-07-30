bfcd02c048c1aebe11b6e1effeb444a18eccc5ff
mJiga <guillermojiga10@gmail.com>
2024-07-30 15:10:36 -0600

    improved search/form, still no functionality

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,27 +2,49 @@ import { Search } from 'lucide-react';
 
 function SearchForm() {
   return (
-    <div className="border-2 border-opacity-60 border-zinc-300 relative flex items-center max-w-4xl h-20 rounded-3xl shadow-lg">
-            <div className="flex-1 px-6 border-r border-zinc-300">
-                <span className="flex flex-col text-sm font-extrabold">Destination</span>
-                <span className="text-xs font-medium text-zinc-500">Where to go?</span>
-            </div>
-            <div className="flex-1 px-6 border-r border-zinc-300">
-            <span className="flex flex-col text-sm font-extrabold">Arrival</span>
-            <span className="text-xs font-medium text-zinc-500">-- / -- / --</span>
-            </div>
-            <div className="flex-1 px-6 border-r border-zinc-300">
-                <span className="flex flex-col text-sm font-extrabold">Departure</span>
-                <span className="text-xs font-medium text-zinc-500">-- / -- / --</span>
-            </div>
-            <div className="flex-1 px-6">
-            <span className="flex flex-col text-sm font-extrabold">Guests and Rooms</span>
-            <span className="text-xs font-medium text-zinc-500">Select amount</span>
-            </div>
-            <button className="flex bg-blue-800 text-white font-bold text-base px-6 py-2 rounded-full mr-2 hover:bg-blue-700 transition-colors duration-200">
-                <Search className='mr-1 w-4 stroke-white'/>
-                Search
-            </button>
+    <div className="border-2 border-opacity-60 border-zinc-300 relative flex items-center max-w-4xl h-20 rounded-3xl shadow-lg overflow-hidden">
+      <form className='flex w-full h-full items-center'>
+        <label className="group flex-1 h-full flex flex-col justify-center px-6 border-r border-zinc-300 hover:bg-zinc-100 transition-colors ease-in-out duration-300 cursor-pointer">
+          <span className="text-sm font-extrabold">Destination</span>
+          <input
+            placeholder='Where to go?'
+            className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
+            required
+          />
+        </label>
+        
+        <label className="group flex-1 h-full flex flex-col justify-center px-6 border-r border-zinc-300 hover:bg-zinc-100 transition-colors ease-in-out duration-300 cursor-pointer">
+          <span className="text-sm font-extrabold">Arrival</span>
+          <input
+            placeholder='-- / -- / --'
+            className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
+            required
+          />
+        </label>
+
+        <label className="group flex-1 h-full flex flex-col justify-center px-6 border-r border-zinc-300 hover:bg-zinc-100 transition-colors ease-in-out duration-300 cursor-pointer">
+          <span className="text-sm font-extrabold">Departure</span>
+          <input
+            placeholder='-- / -- / --'
+            className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
+            required
+          />
+        </label>
+        
+        <label className="group flex-1 h-full flex flex-col justify-center px-6 hover:bg-zinc-100 transition-colors ease-in-out duration-300 cursor-pointer">
+          <span className="text-sm font-extrabold">Guests & Rooms</span>
+          <input
+            placeholder='Select amount'
+            className='text-xs font-medium text-zinc-500 bg-transparent focus:outline-none focus:text-black w-full group-hover:placeholder-zinc-400'
+            required
+          />
+        </label>
+            
+        <button className="h-full bg-blue-800 text-white font-bold text-base px-8 hover:bg-blue-700 transition-colors duration-200 flex items-center justify-center">
+          <Search className='mr-2 w-5 stroke-white'/>
+          Search
+        </button>
+      </form>
     </div>
   )
 }
